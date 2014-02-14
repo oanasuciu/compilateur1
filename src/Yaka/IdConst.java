5f42fd8c33c6fa49e9378f0ac808bfa7c3165466
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2014-02-14 10:04:05 +0100

    IdVar / IdConst

--- src/Yaka/IdConst.java
+++ src/Yaka/IdConst.java
@@ -1,5 +1,13 @@
 package Yaka;
 
 public class IdConst extends Ident {
-
+	private String nom;
+	private String type;
+	private int valeur;
+	
+	public IdConst(String nom, String type, int valeur){
+		this.nom=nom;
+		this.type=type;
+		this.valeur=valeur;
+	}
 }
--- src/Yaka/IdVar.java
+++ src/Yaka/IdVar.java
@@ -1,5 +1,12 @@
 package Yaka;
 
 public class IdVar extends Ident {
-
+	private String nom;
+	private String type;
+	private int valeur;
+	
+	public IdVar(String nom, String type){
+		this.nom=nom;
+		this.type=type;
+	}
 }
