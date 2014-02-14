38bf09933866295c0495f773ad87f44d09c86d0f
hyukchan <hyukchan.kwon@insa-rennes.fr>
2014-02-14 09:09:33 +0100

    TabIdent

--- src/Yaka/TabIdent.java
+++ src/Yaka/TabIdent.java
@@ -1,5 +1,15 @@
 package Yaka;
 
 public class TabIdent {
-
+	private HashYVM<String,Ident> table;
+	
+	public TabIdent(int taille){
+		this.table = new HashYVM<String,Ident>(taille);
+	}
+	
+	public Ident chercheIdent(String clef){...}
+	
+	public boolean existeIdent(String clef){...}
+	
+	public void rangeIdent(String clef, Ident id){...}
 }
