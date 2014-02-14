735d68ede52f962bb50800538fd98671d353c55a
hyukchan <hyukchan.kwon@insa-rennes.fr>
2014-02-14 09:26:35 +0100

    TabIdent Correction des erreurs

--- src/Yaka/TabIdent.java
+++ src/Yaka/TabIdent.java
@@ -1,15 +1,22 @@
 package Yaka;
+import java.util.HashMap;
 
 public class TabIdent {
-	private HashYVM<String,Ident> table;
+	private HashMap<String,Ident> table;
 	
 	public TabIdent(int taille){
-		this.table = new HashYVM<String,Ident>(taille);
+		this.table = new HashMap<String,Ident>(taille);
 	}
 	
-	public Ident chercheIdent(String clef){...}
+	public Ident chercheIdent(String clef){
+		return new Ident();
+	}
 	
-	public boolean existeIdent(String clef){...}
+	public boolean existeIdent(String clef){
+		return false;
+	}
 	
-	public void rangeIdent(String clef, Ident id){...}
+	public void rangeIdent(String clef, Ident id){
+		
+	}
 }
