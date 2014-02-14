cdae967bcc538e16242843216f89981dc75e967c
Oana <osuciu@insa-rennes.fr>
2014-02-14 10:07:11 +0100

    Gestion des déclarations

--- src/Yaka/Declaration.java
+++ src/Yaka/Declaration.java
@@ -1,20 +1,39 @@
 package Yaka;
 
 public class Declaration {
+	/**
+	 * Le dernier type lu dans la grammaire
+	 */
 	private int typeCourant;
 	
 	public Declaration() {
 		
 	}
 	
+	/**
+	 * Met à jour le dernier type lu dans la grammaire
+	 * 
+	 * @param type Une constante représentant le type (entier ou booléen)
+	 */
 	public void metAJourType(int type) {
 		this.typeCourant = type;
 	}
 	
+	/**
+	 * Déclaration d'une nouvelle variable
+	 * 
+	 * @param nom Nom de la variable
+	 */
 	public void nouvelleVariable(String nom) {
-		
+		Ident id = new IdVar(nom, this.typeCourant);
+		Yaka.tabIdent.rangeIdent(nom, id);
 	}
 	
+	/**
+	 * 
+	 * @param nom
+	 * @param valeur
+	 */
 	public void nouvelleConstante(String nom, String valeur) {
 		
 	}
